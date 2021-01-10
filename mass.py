4f2c9289f13577fc69337b46008679f6435c3716
olivervn <45118506+olivervn@users.noreply.github.com>
2021-01-11 02:20:50 +0800

    Delete mass.py

--- mass.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-file = open(sys.argv[1]).read().splitlines()
-for line in file:
-	line = line.split(':')
-	try:
-		os.system('python3 zer0dump.py ' + line[0])
-		pass
-	except KeyboardInterrupt:
-		pass
